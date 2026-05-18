bd5b782060cf770570ff6bc689046bead83d47aa
Luke Dawson (1000141012) <DAWSLJ1@student.op.ac.nz>
2026-05-19 10:49:11 +1200

    First cell code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,67 @@
-﻿namespace PB
+﻿using System;
+using System.Threading;
+
+namespace PB
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Intro()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("You haver been convicted of watching anime on an illegal website."
+                Thread.Sleep(1000);
+            Console.Writeline("The Judge has commited you 25 life sentences due to your severe crime")
+            Console.WriteLine("");
+            Console.Writeline("      -------------Outcast Juvenile Holding Center-------------")
+            Console.Writeline("");
+            Thread.Sleep(3000);
+        }
+
+        public static void Cell()
+        {
+            string celltemp;
+            
+            int celldec;
+            Console.Writeine("You arrive in your cell, a cramped cold and dirty cell")
+                celldec = 0;
+            if (celldec == 0) {
+                Console.WriteLine("What would you like to do");
+                Console.WriteLine("1. Check cell");
+                Console.WriteLine("2. Go to yard");
+                Console.WriteLine("3. Go to cafeteria");
+                Console.WriteLine("4. Go to outside of cell");
+                celltemp = Console.ReadLine();
+                celldec = Convert.ToInt32(celltemp);
+            }
+            if (celldec == 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You look around your cell");
+                Console.WriteLine("It contains the basic things you would expect to find in a cell")
+                    Console.WriteLine("A bed, a desk, a toilet, as well as a window to see the outside");
+                Console.WriteLine("Probably there to tease prisoners");
+                Console.WriteLine("What do you want to do");
+                Console.WriteLine("1. Look at bed");
+                Console.WriteLine("2. Look at toilet");
+                Console.WriteLine("3. Look at desk");
+                Console.WriteLine("4. Look through window");
+                celltemp = Console.ReadLine();
+                celldec = Convert.ToInt32(celltemp);
+            }
+            if (celldec == 2)
+            {
+                SceneYard();
+            }
+            if (celldec == 3)
+            {
+                SceneCafeteria();
+            }
+            if (celldec == 4) 
+            {
+                SceneOC();
+            }
+
+            
+
         }
     }
 }
