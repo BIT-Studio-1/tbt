f9b663aaa3023ba137757e21ff989ff47adfea14
Luke Dawson (1000141012) <DAWSLJ1@student.op.ac.nz>
2026-05-21 16:38:45 +1200

    Intro cleanup

--- Program.cs
+++ Program.cs
@@ -113,6 +113,8 @@ namespace PB
             Console.WriteLine("You have been convicted of watching anime on an illegal website.");
             Thread.Sleep(1000);
             Console.WriteLine("The Judge has commited you 25 life sentences due to your severe crime");
+            Thread.Sleep(1000);
+            Console.WriteLine("");
             Console.WriteLine("");
             Console.WriteLine("      -------------Outcast Juvenile Holding Center-------------");
             Console.WriteLine("");
